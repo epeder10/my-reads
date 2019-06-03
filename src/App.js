57b35710946834203f165dc971c503b642f37897
Eric Pedersen <epeder10@gmail.com>
2019-06-02 21:27:03 -0400

    BUG: route is working correct

--- src/App.js
+++ src/App.js
@@ -9,12 +9,6 @@ import './App.css'
 
 class BooksApp extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
     books: [],
     bookSearch: []
   }
@@ -103,7 +97,7 @@ class BooksApp extends React.Component {
               </div>
             </div>
             <div className="open-search">
-              <NavLink to="/search" className='open-search-button'>Add a book</NavLink>
+              <NavLink to="/search" className='open-search-button' onClick={() => this.setState({bookSearch: []})}>Add a book</NavLink>
             </div>
           </div>
         )}/>
