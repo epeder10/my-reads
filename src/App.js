9c9ef00d5f05d2dd6d33e4a2662c74188cd04d58
Eric Pedersen <epeder10@gmail.com>
2019-06-03 21:52:44 -0400

    Requested changes from code review.

--- src/App.js
+++ src/App.js
@@ -3,26 +3,41 @@ import * as BooksAPI from './BooksAPI'
 import BookShelf from './BookShelf'
 import Book from './Book'
 import { Link } from 'react-router-dom'
-import { NavLink } from 'react-router-dom'
 import { Route } from 'react-router-dom'
+import {debounce} from 'throttle-debounce';
 import './App.css'
 
 class BooksApp extends React.Component {
   state = {
     books: [],
-    bookSearch: []
+    bookSearch: [],
+    searchText: ""
   }
-
+  constructor() {
+    super();
+    this.callAjax = debounce(200, this.callAjax);
+  }
+  search(e) {
+    this.callAjax(e.target.value);
+  }
+  callAjax(value) {
+    this.setState({
+      searchText: value
+    })
+    BooksAPI.search(value).then((book) => 
+    {
+      if (!Array.isArray(book)){
+        book = []
+      }
+      this.setState({
+        bookSearch: book
+    })})
+  }
   componentDidMount() {
     BooksAPI.getAll().then((book) => this.setState({
       books: book
     }));
   }
-
-  queryBooks = (query) => {
-    BooksAPI.search(query).then((book) => this.setState({
-      bookSearch: book
-    }))}
 
   isBookOnShelf = (inBook) => {
     let book = this.state.books.filter(book => book.id === inBook.id)
@@ -43,6 +58,8 @@ class BooksApp extends React.Component {
       tmpBooks.push(book)
     }
     
+    BooksAPI.update(book, newShelf)
+
     this.setState((currentState) => ({
       books: tmpBooks
   }))}
@@ -63,18 +80,19 @@ class BooksApp extends React.Component {
                   However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
                   you don't find a specific author or title. Every search is limited by search terms.
                 */}
-                <input type="text" placeholder="Search by title or author" onChange={(event) => this.queryBooks(event.target.value)}/>
+                <input type="text" placeholder="Search by title or author" onKeyUp={this.search.bind(this)}/>
 
               </div>
             </div>
             <div className="search-books-results">
+              {this.state.searchText.length > 0 && this.state.bookSearch.length === 0 ? <p>No Results</p>:
               <ol className="books-grid">
                 {this.state.bookSearch && this.state.bookSearch.length > 0 && this.state.bookSearch.map(book =>
                   <li key={book.id}>
                       <Book onUpdateBook={this.updateBook} isBookOnShelf={this.isBookOnShelf} book={book}/>
                   </li>
                 ) }
-              </ol>
+              </ol>}
             </div>
           </div>
           )} />
@@ -97,7 +115,7 @@ class BooksApp extends React.Component {
               </div>
             </div>
             <div className="open-search">
-              <NavLink to="/search" className='open-search-button' onClick={() => this.setState({bookSearch: []})}>Add a book</NavLink>
+              <Link to="/search" className='open-search-button' >Add a book</Link>
             </div>
           </div>
         )}/>
--- src/BookShelfChanger.js
+++ src/BookShelfChanger.js
@@ -1,10 +1,8 @@
 import React from 'react'
-import * as BooksAPI from './BooksAPI'
 
 class BookShelfChanger extends React.Component {
     updateBook = (book, shelf) => { 
         this.props.onUpdateBook(book, shelf) 
-        BooksAPI.update(book, shelf)
     }
     
     render() {
